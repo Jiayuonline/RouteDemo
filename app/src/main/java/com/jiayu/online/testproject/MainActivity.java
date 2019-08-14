d375c7248d1aa1abfe94ae9ecac44ac19f37889d
wb <bing.wang@jiayuonline.com>
2019-08-14 14:39:00 +0800

    first

--- app/src/main/java/com/jiayu/online/testproject/MainActivity.java
+++ app/src/main/java/com/jiayu/online/testproject/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         btn_main.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i    = new Intent(MainActivity.this, RouteListActivity.class);
+                Intent i    = new Intent(MainActivity.this,  RouteListActivity.class);
                 startActivity( i);
 
             }
