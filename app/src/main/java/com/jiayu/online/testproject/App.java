16f80d50f5cfc11cb4a424e0b40a49123e5c5bd8
wb <bing.wang@jiayuonline.com>
2019-08-14 13:26:45 +0800

    first commit

--- app/src/main/java/com/jiayu/online/testproject/App.java
+++ app/src/main/java/com/jiayu/online/testproject/App.java
@@ -2,6 +2,7 @@ package com.jiayu.online.testproject;
 
 import android.app.Application;
 
+import com.amap.api.maps.AMap;
 import com.jiayu.commonbase.base.BaseApplication;
 import com.jiayu.commonbase.manager.TaotutuManager;
 
@@ -9,6 +10,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
         TaotutuManager.init(this);
         TaotutuManager.setAccessSecret("56cbaf232dbd48d9b6b7b88760b5784e");
         TaotutuManager.setAccessKey("7b51de35ba4f465a81c48af594df38e1");
--- app/src/main/java/com/jiayu/online/testproject/MainActivity.java
+++ app/src/main/java/com/jiayu/online/testproject/MainActivity.java
@@ -3,13 +3,16 @@ package com.jiayu.online.testproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.amap.api.maps.model.LatLng;
+import com.jiayu.online.taotutu_route.activity.NavActivity;
 import com.jiayu.online.taotutu_route.activity.RouteListActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +25,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+
     }
 }
