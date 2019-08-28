f0a5e087f6423d727625e63e209d0197b8361912
wb <bing.wang@jiayuonline.com>
2019-08-28 10:59:41 +0800

    数据接口

--- app/src/main/java/com/jiayu/online/testproject/App.java
+++ app/src/main/java/com/jiayu/online/testproject/App.java
@@ -2,10 +2,8 @@ package com.jiayu.online.testproject;
 
 import android.app.Application;
 
-import com.amap.api.maps.AMap;
 import com.jiayu.commonbase.base.BaseApplication;
 import com.jiayu.commonbase.manager.TaotutuManager;
-import com.jiayu.online.taotutu_tts.TTSManager;
 
 public class App extends Application {
     @Override
--- app/src/main/java/com/jiayu/online/testproject/MainActivity.java
+++ app/src/main/java/com/jiayu/online/testproject/MainActivity.java
@@ -7,10 +7,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import com.amap.api.maps.model.LatLng;
-import com.jiayu.commonbase.manager.TaotutuManager;
-import com.jiayu.online.taotutu_route.activity.NavActivity;
 import com.jiayu.online.taotutu_route.activity.RouteListActivity;
+import com.jiayu.online.taotutu_route.bean.RouteDetailBean;
+import com.jiayu.online.taotutu_route.bean.RouteListBean;
+import com.jiayu.online.taotutu_route.bean.TTSPointBean;
+import com.jiayu.online.taotutu_route.manager.TaoRouteManager;
+import com.jiayu.online.taotutu_route.presenter.RouteDetailPresenter;
+import com.jiayu.online.taotutu_route.presenter.RouteListPresenter;
+import com.youth.banner.Banner;
+
+import java.util.List;
+
+import retrofit2.Retrofit;
+
 
 public class MainActivity extends AppCompatActivity {
     String TAG = "MainActivity";
@@ -24,11 +33,62 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent i    = new Intent(MainActivity.this,  RouteListActivity.class);
                 startActivity( i);
-
             }
         });
 
+        final TaoRouteManager routeManager = new TaoRouteManager();
 
+        Button btnList = findViewById(R.id.btn_list);
+        btnList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                routeManager.getRouteList(1, 10, new RouteListPresenter.OnRouteListCallback() {
+                    @Override
+                    public void onSuccess(List<RouteListBean> list) {
+                    }
+
+                    @Override
+                    public void onFailed(Throwable throwable) {
+
+                    }
+                });
+            }
+        });
+
+        Button btnDetail = findViewById(R.id.btn_detail);
+        btnDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                routeManager.getRouteDetail("r201811002", new RouteDetailPresenter.OnDetailCallback() {
+                    @Override
+                    public void onSuccess(RouteDetailBean routeDetailBean) {
+                    }
+
+                    @Override
+                    public void onFailed(Throwable throwable) {
+
+                    }
+                });
+            }
+        });
+        Button btnTTS = findViewById(R.id.btn_tts);
+
+        btnTTS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                routeManager.getTTSList("121.388467,31.176875", 1, 10, new RouteDetailPresenter.OnTTSCallback() {
+                    @Override
+                    public void onSuccess(List<TTSPointBean> list) {
+
+                    }
+
+                    @Override
+                    public void onFailed(Throwable throwable) {
+
+                    }
+                });
+            }
+        });
 
     }
 }
